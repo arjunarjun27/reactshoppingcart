a7f705d395d726d90eac8d64f73c9a356567439c
arjun <arjunthestar007@gmail.com>
2019-11-24 01:18:40 -0800

    product list

---
+++ src/component/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Navbar = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+
+};
+
+export default Navbar;
